63e7b8deffca722fb9722cf5783d980cac66ff77
Laura Avila <lavila@bigwise.com>
2025-04-25 16:13:35 -0400

    FEAT: MultiBlocProvider add to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:local_push_notifications_flutter/config/router/app_router.dart';
 import 'package:local_push_notifications_flutter/config/theme/app_theme.dart';
 
+import 'package:local_push_notifications_flutter/presentation/blocs/notifications/notifications_bloc.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(MultiBlocProvider(providers: [
+    BlocProvider(create: (_) => NotificationsBloc())
+  ], child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
