95a8f3c1fe88dfef6c51a19b089aad25128bddf5
Laura Avila <lavila@bigwise.com>
2025-04-25 16:25:28 -0400

    FEAT: requestPermission in iconButton on appbar

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:local_push_notifications_flutter/presentation/blocs/notifications/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,11 +9,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('permissions'),
+        title: context.select((NotificationsBloc bloc) => Text(bloc.state.status.toString())),
         actions: [
           IconButton(
             onPressed: () {
-              // TODO: notifications permissions
+              context.read<NotificationsBloc>().requestPermission();
             },
             icon: const Icon(Icons.settings),
           )
