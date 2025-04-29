f20c82a069f11bde6bec425d68d7f4443963e5f8
Laura Avila <lavila@bigwise.com>
2025-04-29 10:56:39 -0400

    FEAT: get token

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -12,10 +12,11 @@ part 'notifications_state.dart';
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-  NotificationsBloc() : super(NotificationsState()){
+  NotificationsBloc() : super(NotificationsState()) {
     on<NotificationStatusChanged>(_notificationStatusChanged);
-  }
 
+    _initialStatusCheck();
+  }
 
   static Future<void> initializeFCM() async {
     await Firebase.initializeApp(
@@ -23,8 +24,24 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     );
   }
 
-  void _notificationStatusChanged(NotificationStatusChanged event, Emitter<NotificationsState> emit){
+  void _notificationStatusChanged(
+    NotificationStatusChanged event,
+    Emitter<NotificationsState> emit,
+  ) {
     emit(state.copyWith(status: event.status));
+    _getFCMToken();
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+    add(NotificationStatusChanged(settings.authorizationStatus));
+    _getFCMToken();
+  }
+
+  void _getFCMToken() async {
+    if (state.status != AuthorizationStatus.authorized) return;
+    final token = await messaging.getToken();
+    print(token);
   }
 
   void requestPermission() async {
