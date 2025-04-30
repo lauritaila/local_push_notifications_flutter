2ca052b58ab32e487a468f948cceca7b72487d5b
Laura Avila <lavila@bigwise.com>
2025-04-30 15:10:03 -0400

    FEAT: foreground and backgroun FCM configuration

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -9,13 +9,24 @@ import 'package:local_push_notifications_flutter/firebase_options.dart';
 part 'notifications_event.dart';
 part 'notifications_state.dart';
 
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(NotificationsState()) {
     on<NotificationStatusChanged>(_notificationStatusChanged);
 
+    //verify permission
     _initialStatusCheck();
+    //listen to foreground messages
+    _onForegroundMessage();
   }
 
   static Future<void> initializeFCM() async {
@@ -42,6 +53,17 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     if (state.status != AuthorizationStatus.authorized) return;
     final token = await messaging.getToken();
     print(token);
+  }
+
+  void _handleRemoteMessage(RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+    if (message.notification == null) return;
+    print('Message also contained a notification: ${message.notification}');
+  }
+
+    void _onForegroundMessage() async {
+    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
   }
 
   void requestPermission() async {
