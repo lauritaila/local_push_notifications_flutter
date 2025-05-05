69fd45da9433165ebd3a1bf21ea8aa284281547d
Laura Avila <lavila@bigwise.com>
2025-05-05 16:12:46 -0400

    FEAT: Add page details in router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
+import 'package:local_push_notifications_flutter/presentation/screens/details_screen.dart';
 import 'package:local_push_notifications_flutter/presentation/screens/home_screen.dart';
 
 final appRouter = GoRouter(routes: [
   GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+  GoRoute(path: '/push-details/:messageId', builder: (context, state) => DetailsScreen(pushMessageId: state.pathParameters['messageId'] ?? '')),
 ]);
