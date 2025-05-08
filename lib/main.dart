34879bebf99b7fc90eee0bd7f352908bd7568e0c
Laura Avila <lavila@bigwise.com>
2025-05-08 16:18:41 -0400

    FEAT: initialize local notifications

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:local_push_notifications_flutter/config/router/app_router.dart';
 import 'package:local_push_notifications_flutter/config/theme/app_theme.dart';
 
+import 'package:local_push_notifications_flutter/config/local_notification/local_notification.dart';
 import 'package:local_push_notifications_flutter/presentation/blocs/notifications/notifications_bloc.dart';
 
 void main() async {
@@ -12,6 +13,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   await NotificationsBloc.initializeFCM();
+  await LocalNotification.initializeLocalNotification();
 
   runApp(MultiBlocProvider(providers: [
     BlocProvider(create: (_) => NotificationsBloc())
