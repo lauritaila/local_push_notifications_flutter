4a06052b605c1e107b42adc73fb0513392dd3c22
Laura Avila <lauritaila10gmail.com>
2025-06-18 20:50:15 -0400

    REFAC: changued sendDate to sentDate

--- lib/domain/entities/push_messages.dart
+++ lib/domain/entities/push_messages.dart
@@ -2,16 +2,16 @@ class PushMessage {
   final String messageId;
   final String title;
   final String body;
-  final DateTime sendDate;
+  final DateTime sentDate;
   final Map<String, dynamic>? data;
   final String? imageUrl;
 
   PushMessage({
-    required this.messageId,
-    required this.title,
-    required this.body,
-    required this.sendDate,
-    this.data,
+    required this.messageId, 
+    required this.title, 
+    required this.body, 
+    required this.sentDate, 
+    this.data, 
     this.imageUrl
   });
 
@@ -22,9 +22,9 @@ class PushMessage {
       id:        $messageId 
       title:     $title
       body:      $body 
-      sendDate:  $sendDate
+      sendDate:  $sentDate
       data:      $data
       imageUrl:  $imageUrl
-      ''';
+''';
   }
 }
